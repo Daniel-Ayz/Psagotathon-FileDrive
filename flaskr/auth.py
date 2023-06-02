14dba9f27f15878ea9a65a18949dad96c71cc7d8
ayzendan <ayzendan@post.bgu.ac.il>
2023-06-02 09:29:15 +0300

    fixed changes

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -59,7 +59,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('home'))
+            return redirect(url_for('index'))
 
         flash(error)
 
@@ -81,7 +81,7 @@ def load_logged_in_user():
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('home'))
+    return redirect(url_for('index'))
 
 
 def login_required(view):
